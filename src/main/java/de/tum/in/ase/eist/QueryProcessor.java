edf23401fc0a30f6485eea2e512f041f51674378
Göktuğ Açık <goktugacik@gmail.com>
2022-07-14 17:21:43 +0200

    return id in the request

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,7 +12,8 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "MyTeam";
+
+           return query.split(":")[0];
         } else { // TODO extend the programm here
             return "";
         }
--- src/main/java/de/tum/in/ase/eist/WebPageResource.java
+++ src/main/java/de/tum/in/ase/eist/WebPageResource.java
@@ -26,4 +26,5 @@ public class WebPageResource {
 		return "answer";
 
 	}
+
 }
